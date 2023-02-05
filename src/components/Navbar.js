2fa63480dc3eb428df3a6bdcf249440eb54d590d
Albert <asamo7@gmail.com>
2023-02-05 19:21:57 +0000

    Cambio logo y eliminada imagen y link a GitHub

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
-import github from "../img/github-icon.svg";
+//import github from "../img/github-icon.svg";
 import logo from "../img/logo.svg";
 
 const Navbar = class extends React.Component {
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+              <img src={logo} alt="Basquet Femení Cornellà" style={{ width: "88px" }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -79,7 +79,7 @@ const Navbar = class extends React.Component {
                 Form Examples
               </Link>
             </div>
-            &lt;!--
+            {/*
             <div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
@@ -91,7 +91,7 @@ const Navbar = class extends React.Component {
                   <img src={github} alt="Github" />
                 </span>
               </a>
-            </div>--&gt;
+            </div>*/}
           </div>
         </div>
       </nav>
